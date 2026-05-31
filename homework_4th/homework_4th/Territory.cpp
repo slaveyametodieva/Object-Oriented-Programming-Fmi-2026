fc75db41d2f618a518b82f27030d4a85e3a6d4f1
slaveyametodieva <slaveyamil@abv.bg>
2026-05-31 10:43:42 +0300

    commandFactory

--- homework_4th/homework_4th/Territory.cpp
+++ homework_4th/homework_4th/Territory.cpp
@@ -26,3 +26,34 @@ std::unique_ptr<Landmark> Territory::removeLandmark(const std::string& name)
 	throw std::invalid_argument("The name was not found!");
 
 }
+
+void Territory::setDangerLever(unsigned int inpDangerLevel)
+{
+	if (inpDangerLevel > MAX_DANGEER)
+	{
+		throw std::invalid_argument("The danger can not be more than 10!");
+	}
+
+	dangerLevel = inpDangerLevel;
+}
+
+bool Territory::hasLandmark(const std::string& name) const
+{
+	auto findIt = std::find_if(landmarks.begin(), landmarks.end(),
+		[&name](const std::unique_ptr<Landmark>& currL) {return currL->getName() == name; });
+
+	return findIt != landmarks.end();
+}
+
+void Territory::print() const
+{
+	for (const auto& currL : landmarks)
+	{
+		currL->print();
+	}
+}
+
+unsigned int Territory::getDangerLevel() const
+{
+	return dangerLevel;
+}
--- homework_4th/homework_4th/Territory.h
+++ homework_4th/homework_4th/Territory.h
@@ -4,17 +4,21 @@
 #include <vector>
 #include <string>
 #include <stdexcept>
+
+constexpr int MAX_DANGEER = 10;
 class Territory
 {
 	private:
 		std::vector<std::unique_ptr<Landmark>> landmarks;
+		unsigned int dangerLevel;
 
 	public:
 		Territory(std::vector<std::unique_ptr<Landmark>> inpLandmarks);
 		void addLandmark(std::unique_ptr<Landmark> joiningLandmark);
 		std::unique_ptr<Landmark> removeLandmark(const std::string& name);
 		void setDangerLever(unsigned int inpDangerLevel);
-		bool hasLandmark(const std::string& name);
+		bool hasLandmark(const std::string& name) const;
 		void print() const;
+		unsigned int getDangerLevel() const;
 };
 
